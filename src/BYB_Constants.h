525084a4f705cf760b3c48a713ee8acb40629985
Roy Macdonald <macdonald.roy@gmail.com>
2015-10-10 03:38:51 -0400

    added constants files added peakDetector.cpp//implementation not finished

---
+++ src/BYB_Constants.h
@@ -0,0 +1,13 @@
+//
+//  BYB_Constants.h
+//  BYB_HackerHand_Control
+//
+//  Created by Roy Macdonald on 10-10-15.
+//
+//
+
+#ifndef BYB_HackerHand_Control_BYB_Constants_h
+#define BYB_HackerHand_Control_BYB_Constants_h
+
+
+#endif
---
+++ src/peakDetector.cpp
@@ -0,0 +1,9 @@
+//
+//  peakDetector.cpp
+//  BYB_HackerHand_Control
+//
+//  Created by Roy Macdonald on 10-10-15.
+//
+//
+
+#include <stdio.h>
